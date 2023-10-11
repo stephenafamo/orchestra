stephenafamo/orchestra

commit ac8b89acaec50201c161760850fa43206eeb4fde

Make ServerPlayer timeout configurable

server.go
@@ -10,6 +10,7 @@ import (
 // ServerPlayer is a type that extends the *http.Server
 type ServerPlayer struct {
 	*http.Server
+	Timeout time.Duration
 }
 
 // Play starts the server until the context is done
@@ -26,7 +27,12 @@ func (s ServerPlayer) Play(ctxMain context.Context) error {
 
 	select {
 	case <-ctxMain.Done():
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		timeout := s.Timeout
+		if timeout == 0 {
+			timeout = 10 * time.Second
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
 
 		err := s.Shutdown(ctx)

commit d390ea3900c744b9b9d1265ec2b2a77630024d94

Introduce NewServerPlayer
Commit ac8b89a introduced a breaking change and making it harder to create a `ServerPlayer`. This change will allow users of this package to initiate a `ServerPlayer` with forward compatibility with ease.

server.go
@@ -13,6 +13,33 @@ type ServerPlayer struct {
 	Timeout time.Duration
 }
 
+// ServerPlayerOption is a function interface to configure the ServerPlayer
+type ServerPlayerOption func(s *ServerPlayer)
+
+func NewServerPlayer(opts ...ServerPlayerOption) *ServerPlayer {
+	s := &ServerPlayer{
+		Timeout: 10 * time.Second,
+	}
+	for _, f := range opts {
+		f(s)
+	}
+	return s
+}
+
+// WithTimeout replaces the default timeout of ServerPlayer
+func WithTimeout(timeout time.Duration) ServerPlayerOption {
+	return func(s *ServerPlayer) {
+		s.Timeout = timeout
+	}
+}
+
+// WithHTTPServer allow configuring the http.Server of ServerPlayer
+func WithHTTPServer(srv *http.Server) ServerPlayerOption {
+	return func(s *ServerPlayer) {
+		s.Server = srv
+	}
+}
+
 // Play starts the server until the context is done
 func (s ServerPlayer) Play(ctxMain context.Context) error {
 	errChan := make(chan error, 1)
@@ -28,9 +55,6 @@ func (s ServerPlayer) Play(ctxMain context.Context) error {
 	select {
 	case <-ctxMain.Done():
 		timeout := s.Timeout
-		if timeout == 0 {
-			timeout = 10 * time.Second
-		}
 
 		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()

server_test.go
@@ -1,0 +1,20 @@
+package orchestra
+
+import (
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestNewServerPlayer(t *testing.T) {
+	srv := NewServerPlayer(
+		WithHTTPServer(&http.Server{Addr: "localhost:4321"}),
+		WithTimeout(5*time.Second),
+	)
+	if srv.Addr != "localhost:4321" {
+		t.Errorf(`expected srv.Addr to be "localhost:4321", got: %s`, srv.Addr)
+	}
+	if srv.Timeout != (time.Second * 5) {
+		t.Errorf(`expected srv.Timeout to be "5s", got: %s`, srv.Timeout)
+	}
+}